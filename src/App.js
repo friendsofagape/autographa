99aed768f68d16a22249dfb23a7d2ecd3ddbcdac
Ashersam <ashersamuel06@gmail.com>
2020-09-03 14:21:01 +0530

    App setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
 import Main from "./components/main";
-import SetupContextProvider from "./contexts/SetupContext";
-import SettingContextProvider from "./contexts/SettingContext";
 
 function App() {
-  return (
-    <SetupContextProvider>
-      <SettingContextProvider>
-        <Main />
-      </SettingContextProvider>
-    </SetupContextProvider>
-  );
+  return <Main />;
 }
 
 export default App;
