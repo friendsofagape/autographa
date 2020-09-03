249909e0ca3077bd34da642e4b41d4b162329896
Ashersam <ashersamuel06@gmail.com>
2020-09-03 15:03:24 +0530

    changed scripts on new app setup

--- main.js
+++
@@ -1,6 +0,0 @@
-/*
- * TRICKY: this root main.js is used by electron to attach to the development
- * server. e.g. with `yarn start`.
- * The main.js inside the `public/` folder is used by electron after building.
- */
-require("./public/electron");
