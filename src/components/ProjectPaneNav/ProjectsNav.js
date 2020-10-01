2ccccd40e2800ef62e8dd598a6fcbd84300857ed
vipinpaul <vipin.paul@bridgeconn.com>
2020-10-01 15:42:18 +0530

    Linked Sync with Project navigation

--- src/components/ProjectPaneNav/ProjectsNav.js
+++ src/components/ProjectPaneNav/ProjectsNav.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import CreateProjectAccordions from "../CreateProject/CreateProjectAccordions";
 import Profile from "../ProjectsPane/Profile";
 import TableData from "../ProjectsPane/TableData";
+import Sync from "../Sync/Sync";
 
 export const ProjectsNav = ({ title }) => {
   return (
@@ -10,6 +11,8 @@ export const ProjectsNav = ({ title }) => {
         switch (title) {
           case "Profile":
             return <Profile />;
+          case "Sync":
+            return <Sync />;
           case "Projects":
             return <TableData />;
           case "Create New Project":
