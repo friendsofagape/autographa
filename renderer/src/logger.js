3bf564e54f6f6cf634163374238531d6c315271f
Ashersam <ashersamuel06@gmail.com>
2020-10-17 14:10:54 +0530

    handling node based logger while inside browser

--- renderer/src/logger.js
+++ renderer/src/logger.js
@@ -1,34 +1,43 @@
+const isElectron = require('is-electron');
+
 let log;
 if (process.env.NODE_ENV === 'test') {
   // eslint-disable-next-line global-require
   log = require('electron-log');
-} else {
+} else if (isElectron()) {
   log = global.log;
 }
 
 export const logger = (level, filename, text) => {
-  log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
-  log.transports.file.file = 'aglogger.log';
-  log.transports.file.maxSize = 15 * 1024 * 1024;
-  log.transports.file.streamConfig = { encoding: 'utf8', flags: 'a' };
-  log.transports.console.level = false;
-  log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
+  if (isElectron()) {
+    log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
+    log.transports.file.file = 'aglogger.log';
+    log.transports.file.maxSize = 15 * 1024 * 1024;
+    log.transports.file.streamConfig = { encoding: 'utf8', flags: 'a' };
+    log.transports.console.level = false;
+    log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
 
-  switch (level) {
-    case 'error':
-      log.error(`${filename}: ${text}`);
-      break;
-    case 'warn':
-      log.warn(`${filename}: ${text}`);
-      break;
-    case 'info':
-      log.info(`${filename}: ${text}`);
-      break;
-    case 'debug':
-      log.debug(`${filename}: ${text}`);
-      break;
-    default:
-      return null;
+    switch (level) {
+      case 'error':
+        log.error(`${filename}: ${text}`);
+        break;
+      case 'warn':
+        log.warn(`${filename}: ${text}`);
+        break;
+      case 'info':
+        log.info(`${filename}: ${text}`);
+        break;
+      case 'debug':
+        log.debug(`${filename}: ${text}`);
+        break;
+      default:
+        return null;
+    }
   }
+  //   else {
+  //     log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
+  //     log.transports.console.level = false;
+  //     log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
+  //   }
   return null;
 };
