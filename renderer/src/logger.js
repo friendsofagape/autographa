037343eeffe210061bd0d70c156c31ac751b2046
Ashersam <ashersamuel06@gmail.com>
2020-10-17 13:55:30 +0530

    added electron log with custom functon to support inside next js

--- renderer/src/logger.js
+++ renderer/src/logger.js
@@ -1,33 +1,34 @@
-// import moment from "moment";
-// let winston;
-// let printf;
-// let app, fs, remote, filepath;
-// const path = require("path");
-//   winston = require("winston");
-//   printf = require("winston").format.printf;
-//   fs = require("fs");
-//   filepath = "Ag-debug.log";
+let log;
+if (process.env.NODE_ENV === 'test') {
+  // eslint-disable-next-line global-require
+  log = require('electron-log');
+} else {
+  log = global.log;
+}
 
-// const myFormat = printf(({ level, message, label, timestamp }) => {
-//   const customMessage = message.split(",");
-//   const fileName = customMessage[0];
-//   const filenameIndex = customMessage.indexOf(customMessage[0]);
-//   customMessage.splice(filenameIndex, 1);
-//   const resultingMessage = customMessage.join(",");
-//   return `${dateFormat()} [${level.toUpperCase()}] ${fileName}:${resultingMessage}`;
-// });
+export const logger = (level, filename, text) => {
+  log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
+  log.transports.file.file = 'aglogger.log';
+  log.transports.file.maxSize = 15 * 1024 * 1024;
+  log.transports.file.streamConfig = { encoding: 'utf8', flags: 'a' };
+  log.transports.console.level = false;
+  log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
 
-// const dateFormat = () => {
-//   let localTime = moment.utc().toDate();
-//   localTime = moment(localTime).format("YYYY-MM-DD HH:mm:ss");
-//   return localTime;
-// };
-// export const logger = winston.createLogger({
-//   transports: [
-//     new winston.transports.File({
-//       level: process.env.NODE_ENV === "production" ? "warn" : "debug",
-//       filename: filepath,
-//       format: winston.format.combine(myFormat),
-//     }),
-//   ],
-// });
+  switch (level) {
+    case 'error':
+      log.error(`${filename}: ${text}`);
+      break;
+    case 'warn':
+      log.warn(`${filename}: ${text}`);
+      break;
+    case 'info':
+      log.info(`${filename}: ${text}`);
+      break;
+    case 'debug':
+      log.debug(`${filename}: ${text}`);
+      break;
+    default:
+      return null;
+  }
+  return null;
+};
