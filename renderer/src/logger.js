39b88decf680bf3089471ad13118c641363f7256
Ashersam <ashersamuel06@gmail.com>
2020-10-19 15:23:45 +0530

    converted logger switch to functions to stick with existing logger calling

--- renderer/src/logger.js
+++ renderer/src/logger.js
@@ -4,11 +4,9 @@ let log;
 if (process.env.NODE_ENV === 'test') {
   // eslint-disable-next-line global-require
   log = require('electron-log');
-} else if (isElectron()) {
-  log = global.log;
-}
+} else if (isElectron()) log = global.log;
 
-export const logger = (level, filename, text) => {
+const logger = () => {
   if (isElectron()) {
     log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
     log.transports.file.file = 'aglogger.log';
@@ -16,28 +14,46 @@ export const logger = (level, filename, text) => {
     log.transports.file.streamConfig = { encoding: 'utf8', flags: 'a' };
     log.transports.console.level = false;
     log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
-
-    switch (level) {
-      case 'error':
-        log.error(`${filename}: ${text}`);
-        break;
-      case 'warn':
-        log.warn(`${filename}: ${text}`);
-        break;
-      case 'info':
-        log.info(`${filename}: ${text}`);
-        break;
-      case 'debug':
-        log.debug(`${filename}: ${text}`);
-        break;
-      default:
-        return null;
-    }
   }
-  //   else {
-  //     log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
-  //     log.transports.console.level = false;
-  //     log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
-  //   }
-  return null;
+  return log;
 };
+export const error = async (filename, text) => {
+  await logger();
+  log.error(`${filename}: ${text}`);
+};
+export const warn = async (filename, text) => {
+  await logger();
+  log.warn(`${filename}: ${text}`);
+};
+export const info = async (filename, text) => {
+  await logger();
+  log.info(`${filename}: ${text}`);
+};
+export const debug = async (filename, text) => {
+  await logger();
+  log.debug(`${filename}: ${text}`);
+};
+export default logger;
+
+// switch (level) {
+//   case 'error':
+//     log.error(`${filename}: ${text}`);
+//     break;
+//   case 'warn':
+//     log.warn(`${filename}: ${text}`);
+//     break;
+//   case 'info':
+//     log.info(`${filename}: ${text}`);
+//     break;
+//   case 'debug':
+//     log.debug(`${filename}: ${text}`);
+//     break;
+//   default:
+//     log.warn(`${filename}: ${text}`);
+// }
+// }
+//   else {
+//     log.transports.file.level = process.env.NODE_ENV === 'production' ? 'warn' : 'debug';
+//     log.transports.console.level = false;
+//     log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s} [{level}] {text}';
+//   }
