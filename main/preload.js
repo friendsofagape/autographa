492a2b5efddab12cbc2ecd4a2c6004fdede58447
Ashersam <ashersamuel06@gmail.com>
2021-03-24 17:11:39 +0530

    deploy error fix with test fix

--- main/preload.js
+++ main/preload.js
@@ -1,9 +1,7 @@
 const { ipcRenderer } = require('electron');
 const log = require('electron-log');
-const { app } = require('electron');
 
 const [value] = process.argv;
-console.log(process.env.APPDATA);
 window.localStorage.setItem('userPath', process.env.APPDATA);
 window.localStorage.setItem('defaultappPath', value);
 // Since we disabled nodeIntegration we can reintroduce
