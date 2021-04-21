16bb0420b90f8e27e893e5a22388aa5febab483c
Arpit Jacob <arpit.jacob@gmail.com>
2021-04-21 11:33:05 +0200

    Package Added - Tailwind Forms - HeadlessUI React Package
    Config Modified
    - Added TailWind Config with customs colors for Autographa
    
    Modules & Pages Added
    - Projects
    
    Static Files
    - Icons Svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './renderer/src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#0068E2',
+      secondary: '#151515',
+      success: '#32C000',
+      error: '#FF4A4A',
+      white: colors.white,
+      gray: colors.trueGray,
+      black: colors.black,
+      green: colors.green,
+      yellow: colors.amber,
+      red: colors.red,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 };
