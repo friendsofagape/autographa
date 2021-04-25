1d8aad6e9a4f8be7dec998a2865b5ff86ab3df7c
Ashersam <ashersamuel06@gmail.com>
2021-04-25 10:04:26 +0530

    code for basic translation helps feature with navigation

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import ProjectContextProvider from '@/components/context/ProjectContext';
 import ReferenceContextProvider from '@/components/context/ReferenceContext';
 import BibleNavigation from '@/modules/biblenavigation/BibleNavigation';
@@ -10,11 +11,20 @@ const TranslationHelps = dynamic(
 );
 const UsfmEditor = dynamic(
   () => import('@/components/EditorPage/UsfmEditor/UsfmEditor'),
+=======
+import dynamic from 'next/dynamic';
+import BibleNavigationInit from '../renderer/src/components/EditorPage/Navigation/BibleNavigationInit';
+import Meta from '../renderer/src/Meta';
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../renderer/src/components/EditorPage/Navigation/BibleNavigationInit'),
+>>>>>>> efc9f6e... code for basic translation helps feature with navigation
   { ssr: false },
 );
 
 const home = () => (
   <div>
+<<<<<<< HEAD
     <Meta />
     <ProjectContextProvider>
       <ReferenceContextProvider>
@@ -25,6 +35,9 @@ const home = () => (
     </ProjectContextProvider>
 
     {/* <DynamicComponentWithNoSSR /> */}
+=======
+    <DynamicComponentWithNoSSR />
+>>>>>>> efc9f6e... code for basic translation helps feature with navigation
   </div>
 );
 
