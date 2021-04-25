35fad9688df805c69a1ce947b01a02224ecf5738
Ashersam <ashersamuel06@gmail.com>
2021-04-25 10:04:26 +0530

    code basic translation helps feature with navigation

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,17 @@
 import BibleNavigation from '@/modules/biblenavigation/BibleNavigation';
+import dynamic from 'next/dynamic';
 import Meta from '../renderer/src/Meta';
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../renderer/src/components/EditorPage/Navigation/BibleNavigationInit'),
+  { ssr: false },
+);
 
 const home = () => (
   <div>
     <Meta />
     <BibleNavigation />
+    <DynamicComponentWithNoSSR />
   </div>
 );
 
---
+++ renderer/src/components/EditorPage/Reference/TranslationHelps.js
@@ -0,0 +1,108 @@
+import { makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Workspace } from 'resource-workspace-rcl';
+import PropTypes from 'prop-types';
+import TranslationHelpsCard from './TranslationHelpsCard';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  dragIndicator: {},
+}));
+const TranslationHelps = ({
+  bookID,
+  currentChapterID,
+  currentVerse,
+}) => {
+  const classes = useStyles();
+  const [currentLayout, setCurrentLayout] = useState(null);
+  const layout = {
+    widths: [
+      [1, 1, 2],
+      [2, 2],
+      [2, 2],
+    ],
+    heights: [[5], [10, 10], [10, 10]],
+  };
+
+  if (currentLayout) {
+    layout.absolute = currentLayout;
+  }
+  return (
+    <>
+      <Workspace
+        rowHeight={25}
+        layout={layout}
+        gridMargin={[15, 15]}
+        classes={classes}
+        onLayoutChange={setCurrentLayout}
+      >
+        <TranslationHelpsCard
+          title="TranslationNotes"
+          verse={currentVerse}
+          chapter={currentChapterID}
+          projectId={bookID || 'mat'}
+          branch="master"
+          languageId="en"
+          resourceId="tn"
+          owner="test_org"
+          server="https://git.door43.org"
+        />
+        <TranslationHelpsCard
+          title="Translation Words List"
+          verse={currentVerse}
+          chapter={currentChapterID}
+          projectId={bookID || 'mat'}
+          branch="master"
+          viewMode="list"
+          languageId="en"
+          resourceId="twl"
+          owner="test_org"
+          server="https://git.door43.org"
+        />
+        <TranslationHelpsCard
+          title="Translation Words"
+          verse={currentVerse}
+          chapter={currentChapterID}
+          projectId={bookID || 'mat'}
+          branch="master"
+          viewMode="markdown"
+          languageId="en"
+          resourceId="twl"
+          owner="test_org"
+          server="https://git.door43.org"
+        />
+        <div style={{
+            float: 'left',
+            marginTop: '450px',
+        }}
+        >
+          <TranslationHelpsCard
+            title="Translation Questions"
+            verse={currentVerse}
+            chapter={currentChapterID}
+            projectId={bookID || 'mat'}
+            branch="master"
+            viewMode="question"
+            languageId="en"
+            resourceId="tq"
+            filePath={null}
+            owner="test_org"
+            server="https://git.door43.org"
+          />
+        </div>
+
+      </Workspace>
+    </>
+    );
+  };
+
+export default TranslationHelps;
+
+TranslationHelps.propTypes = {
+  currentChapterID: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  currentVerse: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  bookID: PropTypes.string.isRequired,
+};
