ea8826e454100d89bee914129d118f4f1e1d62d1
Sarah <sarahkane682@yahoo.com>
2021-05-10 15:56:04 -0700

    profile-page

---
+++ renderer/src/modules/projects/CustomAutocomplete.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import useAutocomplete from '@material-ui/lab/useAutocomplete';
+import NoSsr from '@material-ui/core/NoSsr';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import styled from 'styled-components';
+
+const Label = styled('label')`
+  padding: 8px;
+  display: block;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.025em;
+  color: #0068E2;
+  font-weight: 300;
+
+`;
+
+const InputWrapper = styled('div')`
+  width: 385px;
+  min-height: 40px;
+  border: 2px solid #d9d9d9;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 1px;
+  display: flex;
+  flex-wrap: wrap;
+  
+
+  &:hover {
+    border-color: #0068E2
+  }
+
+  &.focused {
+    border-color: #0068E2
+    box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);
+  }
+
+  & input {
+    font-size: 14px;
+    font-weight: 300;
+    height: 30px;
+    box-sizing: border-box;
+    padding: 4px 6px;
+    width: 0;
+    min-width: 30px;
+    flex-grow: 1;
+    border: 0;
+    margin: 0;
+    outline: 0;
+  }
+`;
+
+const Tag = styled(({ label, onDelete, ...props }) => (
+  <div {...props}>
+    <span>{label}</span>
+    <CloseIcon onClick={onDelete} />
+  </div>
+))`
+  display: flex;
+  align-items: center;
+  height: 24px;
+  margin: 5px;
+  line-height: 22px;
+  background-color: rgba(229, 231, 235);
+  border-radius: 16px;
+  box-sizing: content-box;
+  padding: 0 4px 0 10px;
+  outline: 0;
+  overflow: hidden;
+
+  &:focus {
+    border-color: #40a9ff;
+    background-color: #e6f7ff;
+  }
+
+
+  & span {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 20px;
+    letter-spacing: 0.25px;
+    color: rgba(0, 0, 0, 0.87);
+  }
+
+  & svg {
+    cursor: pointer;
+    padding: 4px;
+  }
+
+`;
+
+const Listbox = styled('ul')`
+  width: 300px;
+  font-weight: 300;
+  margin: 2px 0 0;
+  padding: 0;
+  position: absolute;
+  list-style: none;
+  background-color: #fff;
+  overflow: auto;
+  max-height: 250px;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+  z-index: 1;
+
+  & li {
+    padding: 5px 12px;
+    display: flex;
+
+    & span {
+      flex-grow: 1;
+    }
+
+    & svg {
+      color: transparent;
+    }
+  }
+
+  & li[aria-selected='true'] {
+    background-color: #fafafa;
+    font-weight: 300;
+
+    & svg {
+      color: #1890ff;
+    }
+  }
+
+  & li[data-focus='true'] {
+    background-color: #e6f7ff;
+    cursor: pointer;
+
+    & svg {
+      color: #000;
+    }
+  }
+`;
+
+export default function CustomizedHook({ list, label }) {
+  const {
+    getRootProps,
+    getInputLabelProps,
+    getInputProps,
+    getTagProps,
+    getListboxProps,
+    getOptionProps,
+    groupedOptions,
+    value,
+    focused,
+    setAnchorEl,
+  } = useAutocomplete({
+    id: 'customized-hook-demo',
+    defaultValue: [list[1]],
+    multiple: true,
+    options: list,
+    getOptionLabel: (option) => option.title,
+  });
+
+  return (
+    <NoSsr>
+      <div>
+        <div {...getRootProps()}>
+          <Label {...getInputLabelProps()}>
+            { label }
+          </Label>
+          <InputWrapper ref={setAnchorEl} className={focused ? 'focused' : ''}>
+            {value.map((option, index) => (
+              <Tag label={option.title} {...getTagProps({ index })} />
+            ))}
+
+            <input {...getInputProps()} />
+          </InputWrapper>
+        </div>
+        {groupedOptions.length > 0 ? (
+          <Listbox {...getListboxProps()}>
+            {groupedOptions.map((option, index) => (
+              <li {...getOptionProps({ option, index })}>
+                <span>{option.title}</span>
+                <CheckIcon fontSize="small" />
+              </li>
+            ))}
+          </Listbox>
+        ) : null}
+      </div>
+    </NoSsr>
+  );
+}
---
+++ renderer/src/modules/projects/Profile.js
@@ -0,0 +1,157 @@
+import ProjectLayout from '../../layouts/ProjectsLayout';
+import CustomAutocomplete from './CustomAutocomplete';
+
+const regions = [
+  { title: 'New Delhi, India' },
+  { title: 'Pretoria, South Africa' },
+  { title: 'Dakar, Senegal' },
+];
+
+const languages = [
+  { title: 'Hindu' },
+  { title: 'Zulu' },
+  { title: 'Wolof' },
+];
+
+function InputBar({ title }) {
+    function clearText() {
+      document.getElementById(title).value = '';
+  }
+
+    return (
+      <div>
+        <h4 className="text-xs font-base mb-2 ml-2 text-primary  tracking-wide leading-4  font-light">{title}</h4>
+        <div className=" flex justify-between h-10 w-96 border-2 bg-white  border-gray-300  rounded">
+          <input
+            type="text"
+            className="border-1 border-white  bg-white rounded-full m-1 focus:ring-white focus:border-none font-light"
+            id={title}
+          />
+          <button
+            onClick={clearText}
+            type="button"
+          >
+            <img
+              className=" w-5 h-5 m-2 "
+              src="/illustrations/clear-button.svg"
+              alt="xbutton"
+            />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  export default function UserProfile() {
+      return (
+        <ProjectLayout title="personal information">
+
+          <div className=" bg-gray-100 flex">
+            <div className="w-60 h-screen bg-gray-900 ">
+              <div className="grid grid-rows-5 p-8 gap-16 pb-20 mr-20">
+                <div className="grid grid-cols-2">
+                  <img
+                    className=""
+                    src="/illustrations/green-check.svg"
+                    alt="Workflow"
+                  />
+                  <div className="text-white tracking-wider pl-3"> Name</div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <img
+                    src="/illustrations/step-two.svg"
+                    alt="Workflow1"
+                  />
+                  <div className="text-white tracking-wider  pl-3"> Email</div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <img
+                    src="/illustrations/step-three.svg"
+                    alt="Workflow2"
+                  />
+                  <span className="text-white tracking-wider pl-3 "> Password</span>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <img
+                    src="/illustrations/step-four.svg"
+                    alt="Workflow3"
+                  />
+                  <span className="text-white tracking-wider  pl-3"> Organisation</span>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <img
+                    src="/illustrations/step-five.svg"
+                    alt="Workflow4"
+                  />
+                  <span className="text-white tracking-wider pl-3  "> Region</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full h-auto bg-white ml-5 mt-3 mr-3 mb-3 rounded-lg shadow border-2">
+
+              <form className="grid gap-12 grid-rows-8 pt-5 pl-5">
+                <div className="flex gap-8">
+                  <input
+                    type="text"
+                    name="given-name"
+                    id="name"
+                    autoComplete="given-name"
+                    className="bg-gray-100 w-44 block rounded shadow-sm sm:text-sm focus:ring-gray-500 focus:border-primary border-gray-200 h-10 font-light"
+                  />
+                  <input
+                    type="text"
+                    name="family-name"
+                    id="name"
+                    autoComplete="given-name"
+                    className="bg-gray-100 w-44 h-10  block rounded  sm:text-sm focus:ring-gray-500 focus:border-primary border-gray-200 font-light "
+                  />
+                </div>
+
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="bg-gray-100 w-96 block rounded shadow-sm sm:text-sm focus:ring-gray-500 focus:border-primary border-gray-200 h-10 font-light"
+                />
+
+                <div className="flex gap-3">
+                  <input
+                    className="bg-gray-100 w-96 block rounded shadow-sm sm:text-sm focus:ring-gray-500 focus:border-primary border-gray-200 h-10 font-light"
+                    type="password"
+                  />
+                  <button type="button" onClick="">
+
+                    <img
+                      className=""
+                      src="/illustrations/edit-password.svg"
+                      alt="edit password"
+                    />
+                  </button>
+
+                </div>
+                <InputBar title=" Organisation" />
+                <div>
+                  <CustomAutocomplete list={regions} label="Region" />
+                </div>
+                <div>
+                  <CustomAutocomplete list={languages} label="App Language" />
+                </div>
+
+                <button
+                  type="submit"
+                  className=" w-20 h-9 bg-success  shadow-md font-light text-white border-none text-xs leading-5 rounded uppercase"
+                >
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </ProjectLayout>
+
+      );
+    }
