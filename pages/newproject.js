f7ec03af2f5fc8362b191464a9fd2ba3508ac92b
vipinpaul <vipin.paul@bridgeconn.com>
2021-06-10 11:09:09 +0530

    fix for the build

--- pages/newproject.js
+++ pages/newproject.js
@@ -1,9 +1,15 @@
 import NewProject from '@/modules/projects/NewProject';
 import AuthenticationContextProvider from '@/components/Login/AuthenticationContextProvider';
+import ProjectContextProvider from '@/components/context/ProjectContext';
+import ReferenceContextProvider from '@/components/context/ReferenceContext';
 
 const newproject = () => (
   <AuthenticationContextProvider>
-    <NewProject />
+    <ProjectContextProvider>
+      <ReferenceContextProvider>
+        <NewProject />
+      </ReferenceContextProvider>
+    </ProjectContextProvider>
   </AuthenticationContextProvider>
 );
 
