6e1364bdca97ceb7a486bed85acf17c0a04aecd6
Asher Philip Samuel <ashersamuel06@gmail.com>
2021-08-17 12:22:00 +0530

    Update environment.js

--- renderer/environment.js
+++ renderer/environment.js
@@ -1,6 +1,3 @@
 export const environment = {
     production: false,
-    APPLICATION_ID: 'AutographaEditor',
-    JAVASCRIPT_KEY: 'C3925DFBCF06DF5291AC',
-    SERVER_URL: 'http://dev.autographa.org:1337/parse',
 };
