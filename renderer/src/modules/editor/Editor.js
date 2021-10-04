c003881ddd2dafe13088b703ed9dab14c071867b
Ashersam <ashersamuel06@gmail.com>
2021-10-04 17:35:24 +0530

    usability fixes

--- renderer/src/modules/editor/Editor.js
+++ renderer/src/modules/editor/Editor.js
@@ -219,7 +219,6 @@ export default function Editor({
                                           active ? 'bg-gray-100' : '',
                                           'block ml-5 mx-1 px-2 py-2 text-sm text-gray-700 form-checkbox',
                                         )}
-                                // checked
                               />
                               <span className="ml-3">Verse per line</span>
                             </label>
