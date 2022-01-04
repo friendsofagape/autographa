900f23290c9ec831ab2efa25ef086f4203b20d6e
vipinpaul <vipin.paul@bridgeconn.com>
2022-01-04 11:08:27 +0530

    removing the win menus

--- main/index.js
+++ main/index.js
@@ -15,6 +15,7 @@ function createWindow() {
  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
