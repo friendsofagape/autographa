8497bf34a548961b635cbc803c9adc2bcbec1a77
vipinpaul <vipin.paul@bridgeconn.com>
2022-06-02 01:08:21 +0530

    Reverting back to previous branch

--- renderer/src/core/burrito/importBurrito.js
+++ renderer/src/core/burrito/importBurrito.js
@@ -7,6 +7,8 @@ import { validate } from '../../util/validate';
 import { updateVersion } from './updateTranslationSB';
 
 const md5 = require('md5');
+
+const fs = window.require('fs');
 const path = require('path');
 
 export const checkDuplicate = async (metadata, currentUser, resource) => {
@@ -14,7 +16,6 @@ export const checkDuplicate = async (metadata, currentUser, resource) => {
   const projectName = metadata.identification?.name?.en;
   let existingProject;
   let id;
-  const fs = window.require('fs');
   const newpath = localStorage.getItem('userPath');
   const projectDir = path.join(newpath, 'autographa', 'users', currentUser, resource);
   const folderList = fs.readdirSync(projectDir);
@@ -49,7 +50,6 @@ export const checkDuplicate = async (metadata, currentUser, resource) => {
   return existingProject;
 };
 export const viewBurrito = async (filePath, currentUser, resource) => {
-  const fs = window.require('fs');
   logger.debug('importBurrito.js', 'Inside viewBurrito');
   const result = {};
   if (fs.existsSync(path.join(filePath, 'metadata.json'))) {
@@ -93,7 +93,6 @@ export const viewBurrito = async (filePath, currentUser, resource) => {
 };
 const importBurrito = async (filePath, currentUser, updateBurritoVersion) => {
   logger.debug('importBurrito.js', 'Inside importBurrito');
-  const fs = window.require('fs');
   const fse = window.require('fs-extra');
   const status = [];
   const newpath = localStorage.getItem('userPath');
