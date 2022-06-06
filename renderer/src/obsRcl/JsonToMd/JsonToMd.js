d1da60c8843e53ecb2477733c92f1def4a3d3ab1
siju <siju.moncy@gmail.com>
2022-06-06 13:28:48 +0530

    Fix lint issue in JsonToMd file

--- renderer/src/obsRcl/JsonToMd/JsonToMd.js
+++ renderer/src/obsRcl/JsonToMd/JsonToMd.js
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export default function JsonToMd(story, imageUrl) {
-  const title  = `# ${story.title}\n\n`;
+  const title = `# ${story.title}\n\n`;
   const end = `_${story.end}_`;
-  const body = story.story.reduce((str, value) =>  str +`![OBS Image](${value.url})\n\n${value.text}\n\n`, "");
-  const storyStr = title+body+end;
-  return imageUrl !== ""
-    ? storyStr.replaceAll("https://cdn.door43.org/obs/jpg/360px/", imageUrl)
+  const body = story.story.reduce((str, value) => `${str} ![OBS Image](${value.url})\n\n${value.text}\n\n`, '');
+  const storyStr = title + body + end;
+  return imageUrl !== ''
+    ? storyStr.replaceAll('https://cdn.door43.org/obs/jpg/360px/', imageUrl)
     : storyStr;
 }
 
