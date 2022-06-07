8bb9acb22e47253ffa267a6504b14bd0d811e056
vipinpaul <vipin.paul@bridgeconn.com>
2022-06-07 15:23:03 +0530

    Adding the reference panel for OBS contents

--- renderer/src/components/EditorPage/ObsEditor/EditorPanel.js
+++ renderer/src/components/EditorPage/ObsEditor/EditorPanel.js
@@ -1,6 +1,11 @@
+import { ProjectContext } from '@/components/context/ProjectContext';
+import { ReferenceContext } from '@/components/context/ReferenceContext';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 
 const EditorPanel = ({ obsStory, storyUpdate }) => {
+  const { actions: { setSelectedStory } } = useContext(ReferenceContext);
+  const { states: { scrollLock } } = useContext(ProjectContext);
   const handleChange = (e) => {
     const index = e.target.getAttribute('data-id');
     const value = e.target.value;
@@ -42,6 +47,7 @@ const EditorPanel = ({ obsStory, storyUpdate }) => {
             <textarea
               name={story.title}
               onChange={handleChange}
+              onClick={() => setSelectedStory(scrollLock === true ? 0 : story.id)}
               value={story.title}
               data-id={story.id}
               className="flex-grow text-justify ml-2 p-2 text-sm"
@@ -57,6 +63,7 @@ const EditorPanel = ({ obsStory, storyUpdate }) => {
             <textarea
               name={story.text}
               onChange={handleChange}
+              onClick={() => setSelectedStory(scrollLock === true ? 0 : story.id)}
               value={story.text}
               data-id={story.id}
               className="flex-grow text-justify ml-2 p-2 text-sm"
@@ -72,6 +79,7 @@ const EditorPanel = ({ obsStory, storyUpdate }) => {
             <textarea
               name={story.end}
               onChange={handleChange}
+              onClick={() => setSelectedStory(scrollLock === true ? 0 : story.id)}
               value={story.end}
               data-id={story.id}
               className="flex-grow text-justify ml-2 p-2 text-sm"
