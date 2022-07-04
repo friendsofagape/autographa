1015ad5e5f4dc9bbda5067bd99278f86665941ee
Bobby <bobbykumar706584@gmail.com>
2022-07-04 12:46:03 +0530

    testing

--- e2e-tests/base.test.ts
+++ e2e-tests/base.test.ts
@@ -17,91 +17,95 @@ test('Check for autographa app render', async () => {
 });
 
 // test('Create a new user and Navigate project', async () => {
-// 	await window.fill('#username', 'new user');
+// 	await window.fill('#username', 'newUser');
 // 	await window.click('[data-testid=login-button]');
 // 	const title = await window.textContent('[aria-label=projects]');
 // 	expect(title).toBe('Projects');
 // });
 
 test('Click user and Navigate projects', async () => {
-	await window.click('id=new user');
+	await window.click('#newUser');
 	const title = await window.textContent('[aria-label=projects]');
 	expect(title).toBe('Projects');
 	// await electronApp.close();
 });
 
-test('Fill and test new project page', async () => {
-	// await window.click('id=new user');
-	await window.click('[aria-label=new]');
-	await window.fill('#project_name', 'aatest project');
-	await window.fill('#project_description', 'test version');
-	await window.fill('#version_abbreviated', 'test');
-	await window.click('#open-advancesettings');
-	await window.click('[aria-label=new-testament]');
-	await window.click('[aria-label=close-custombiblenavigation]');
-	await window.click('[aria-label=create]');
-	const title = await window.textContent('[aria-label=projects]');
-	expect(title).toBe('Projects');
-});
+// test('Fill and test new project page', async () => {
+// 	await window.click('#newUser');
+// 	await window.click('[aria-label=new]');
+// 	await window.fill('#project_name', 'latest project');
+// 	await window.fill('#project_description', 'test version');
+// 	await window.fill('#version_abbreviated', 'test');
+// 	await window.click('#open-advancesettings');
+// 	await window.click('[aria-label=new-testament]');
+// 	await window.click('[aria-label=close-custombiblenavigation]');
+// 	await window.click('[aria-label=create]');
+// 	const projectname = await window.innerText('[aria-label=project-name]');
+// 	console.log(projectname);
+// 	expect(projectname).toBe('latest project');
+// });
 
-test('Star the project', async () => {
-	// await window.click('id=new user');
-	await window.click('[aria-label=unstar-project]');
-	const projectname = await window.innerText('[aria-label=project-name]');
-	expect(projectname).toBe('aatest project');
-});
+// test('Star the project', async () => {
+// 	await window.click('#newUser');
+// 	await window.click('[aria-label=unstar-project]');
+// 	const projectname = await window.innerText('[aria-label=project-name]');
+// 	expect(projectname).toBe('latest project');
+// });
 
-test('Unstar the project', async () => {
-	await window.click('[aria-label=star-project]');
-	const projectname = await window.innerText(
-		'[aria-label=unstar-project-name]',
-	);
-	expect(projectname).toBe('aatest project');
-});
+// test('Unstar the project', async () => {
+// 	await window.click('[aria-label=star-project]');
+// 	const projectname = await window.innerText(
+// 		'[aria-label=unstar-project-name]',
+// 	);
+// 	expect(projectname).toBe('latest project');
+// });
 
-test('Search and test for resulting project', async () => {
-	await window.fill('#search_box', 'aatest');
-	const projectname = await window.innerText('[aria-label=star-project]');
-	expect(projectname).toBe('aatest project');
-});
+// test('Search and test for resulting project', async () => {
+// 	await window.fill('#search_box', 'latest');
+// 	const projectname = await window.innerText(
+// 		'[aria-label=unstar-project-name]',
+// 	);
+// 	expect(projectname).toBe('latest project');
+// });
 
-test('Update User profile and Navigate projects', async () => {
-	await window.click('text=Open user menu');
-	await window.click('id=profile');
-	await window.fill('#organisation', 'The vidya foundations');
-	await window.click('button:text("Save")');
-	await window.click('[aria-label=projectList]');
-	const projectname = await window.innerText('[aria-label=star-project]');
-	console.log(projectname);
-	expect(projectname).toBe('aatest project');
-});
+// test('Update User profile and Navigate projects', async () => {
+// 	await window.click('text=Open user menu');
+// 	await window.click('id=profile');
+// 	await window.fill('#organisation', 'The vidya foundations');
+// 	await window.click('button:text("Save")');
+// 	await window.click('[aria-label=projectList]');
+// 	const projectname = await window.innerText('[aria-label=star-project]');
+// 	console.log(projectname);
+// 	expect(projectname).toBe('aatest project');
+// });
 
-test('Test for edit project with updated description', async () => {
-	await window.click('[aria-label=star-expand-project]');
-	await window.click('[aria-label=star-menu-project]');
-	await window.click('[aria-label=edit-project]');
-	await window.fill('#project_description', 'test version edit');
-	await window.click('[aria-label=save-edit-project]');
-	await window.click('[aria-label=cancel-edit-project]');
-	await window.click('[aria-label=expand-project]');
-	const description = await window.textContent(
-		'[aria-label=project-description-display]',
-	);
-	expect(description).toBe('test version edit');
-});
+// test('Test for edit project with updated description', async () => {
+// 	await window.click('[aria-label=star-expand-project]');
+// 	await window.click('[aria-label=star-menu-project]');
+// 	await window.click('[aria-label=edit-project]');
+// 	await window.fill('#project_description', 'test version edit');
+// 	await window.click('[aria-label=save-edit-project]');
+// 	await window.click('[aria-label=cancel-edit-project]');
+// 	await window.click('[aria-label=expand-project]');
+// 	const description = await window.textContent(
+// 		'[aria-label=project-description-display]',
+// 	);
+// 	expect(description).toBe('test version edit');
+// });
 
-test('Sign out and return to Autographa app', async () => {
-	await window.click('text=Open user menu');
-	await window.click('text=Sign out');
-	const title = await window.textContent('text=Sign In');
-	expect(title).toBe('Sign In');
-});
+// test('Sign out and return to Autographa app', async () => {
+// 	await window.click('text=Open user menu');
+// 	await window.click('text=Sign out');
+// 	const title = await window.textContent('text=Sign In');
+// 	expect(title).toBe('Sign In');
+// });
 
-// // test('Click on project to open editor page', async () => {
-// //     await window.click('[aria-label=unstar-projectname]');
-// //     const editorpane = await window.innerText('[aria-label=editor-pane]');
-// //     expect(editorpane).toBe('EDITOR');
-// // });
+test('Click on project to open editor page', async () => {
+	await window.click('[aria-label=unstar-project-name]');
+	const editorpane = await window.innerText('[aria-label=editor-pane]');
+	console.log(editorpane);
+	expect(editorpane).toBe('EDITOR');
+});
 
 // // test('Check project name', async () => {
 // //     const projectname = await window.innerText('[aria-label=editor-project-name]');
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,10 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  testDir: './e2e-tests',
-  maxFailures: 2,
+	testDir: './e2e-tests',
+	maxFailures: 2,
+	timeout: 30000,
+	globalTimeout: 600000,
 };
 
 export default config;
