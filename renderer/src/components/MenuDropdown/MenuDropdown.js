4b65f2177dfa6dba436fe8bd9f88a5072075a7ed
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2022-08-26 10:28:31 -0500

    font detection supporting online in addition to offline

--- renderer/src/components/MenuDropdown/MenuDropdown.js
+++ renderer/src/components/MenuDropdown/MenuDropdown.js
@@ -5,6 +5,7 @@ import {
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 import localforage from 'localforage';
+import { useDetectFonts, fontList as fontsArray } from 'font-detect-rhl';
 import { ReferenceContext } from '../context/ReferenceContext';
 import * as logger from '../../logger';
 
@@ -32,6 +33,42 @@ export default function MenuDropdown() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // Detect fonts with font-detect-rhl:
+  const detectedFonts = useDetectFonts({ fonts: fontsArray });
+
+  // Detected fonts components for MUI Dropdown:
+  const detectedFontsComponents = detectedFonts.map((font, index) => (
+    <Listbox.Option
+      key={index}
+      className={({ active }) => `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
+            cursor-default select-none relative py-2 pl-10 pr-4`}
+      value={font.name}
+      aria-label={font.name}
+    >
+      {({ selectedFont, active }) => (
+        <>
+          <span
+            className={`${
+            selectedFont ? 'font-medium' : 'font-normal'
+          } block truncate`}
+          >
+            {font.name}
+          </span>
+          {selectedFont ? (
+            <span
+              className={`${
+              active ? 'text-amber-600' : 'text-amber-600'
+            }
+                  absolute inset-y-0 left-0 flex items-center pl-3`}
+            >
+              <CheckIcon className="w-5 h-5" aria-hidden="true" />
+            </span>
+        ) : null}
+        </>
+    )}
+    </Listbox.Option>
+  ));  
+
   return (
     <div style={{ width: '150%' }} className="w-100">
       <Listbox value={selectedFont} onChange={setSelectedFont}>
@@ -53,39 +90,7 @@ export default function MenuDropdown() {
           >
 
             <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              { fonts && (
-                fonts.map((font, personIdx) => (
-                  <Listbox.Option
-                    key={personIdx}
-                    className={({ active }) => `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
-                          cursor-default select-none relative py-2 pl-10 pr-4`}
-                    value={font}
-                    aria-label={font}
-                  >
-                    {({ selectedFont, active }) => (
-                      <>
-                        <span
-                          className={`${
-                          selectedFont ? 'font-medium' : 'font-normal'
-                        } block truncate`}
-                        >
-                          {font}
-                        </span>
-                        {selectedFont ? (
-                          <span
-                            className={`${
-                            active ? 'text-amber-600' : 'text-amber-600'
-                          }
-                                absolute inset-y-0 left-0 flex items-center pl-3`}
-                          >
-                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                          </span>
-                      ) : null}
-                      </>
-                  )}
-                  </Listbox.Option>
-              ))
-              )}
+              {detectedFontsComponents}
             </Listbox.Options>
           </Transition>
         </div>
