3f9d907dc40bcf8aaa7cda5cf45466939f672b2c
rivissolutions <72241997+sijumoncy@users.noreply.github.com>
2022-09-07 12:20:05 +0530

    lint ifx

--- renderer/src/components/Sync/Gitea/EditorAutoSync.js
+++ renderer/src/components/Sync/Gitea/EditorAutoSync.js
@@ -382,7 +382,7 @@ function AutoSync({ selectedProject }) {
                     leaveFrom="opacity-100 scale-100"
                     leaveTo="opacity-0 scale-95"
                   >
-                    <Dialog.Panel className="bg-gray-200  w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Panel className="bg-gray-200  w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all">
                       <div className="flex mr-2 justify-between">
                         <div className="">
                           <Dialog.Title
