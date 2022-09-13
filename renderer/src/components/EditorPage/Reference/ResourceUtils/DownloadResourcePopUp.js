fbe6e66575c5780dc50f41131b47d35c62f289ec
rivissolutions <72241997+sijumoncy@users.noreply.github.com>
2022-09-13 18:20:50 +0530

    fetch filtered resources

--- renderer/src/components/EditorPage/Reference/ResourceUtils/DownloadResourcePopUp.js
+++ renderer/src/components/EditorPage/Reference/ResourceUtils/DownloadResourcePopUp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import localforage, * as localForage from 'localforage';
 import { Dialog, Transition } from '@headlessui/react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { SnackBar } from '@/components/SnackBar';
 import {
   Accordion, AccordionDetails, AccordionSummary, Typography,
@@ -54,11 +54,12 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenDonwloadPopUp }) {
-    // const { t } = useTranslation();
+    const { t } = useTranslation();
     const [snackBar, setOpenSnackBar] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
     const [loadFilterDiv, setLoadFilterDiv] = React.useState(false);
     const [snackText, setSnackText] = React.useState('');
+    // eslint-disable-next-line no-unused-vars
     const [notify, setNotify] = React.useState();
 
     const [resourceData, setresourceData] = React.useState([]);
@@ -69,26 +70,52 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
         setIsOpenDonwloadPopUp(false);
       };
 
-    const fetchResource = async () => {
-        setLoading(true);
-        const url = 'https://git.door43.org/api/catalog/v5/search?subject=Aligned%20Bible&subject=Bible&lang=en&lang=ml&lang=hi';
-        await fetch(url)
-        .then((response) => response.json())
-        .then((res) => {
-            // console.log('fetched out res ->', res.data);
-            const temp_resource = {};
-            res.data.forEach((element) => {
-                element.isChecked = false;
-                if (element.language in temp_resource) {
-                    temp_resource[element.language].push(element);
-                } else {
-                    temp_resource[element.language] = [element];
-                }
-            });
-            setresourceData(temp_resource);
-            setLoading(false);
+    const fetchResource = async (filter) => {
+      setLoading(true);
+      // subject = bible and lang = en - if not custom filter or initial loading
+      const baseUrl = 'https://git.door43.org/api/catalog/v5/search';
+      let url = '';
+      if (filter) {
+        url = `${baseUrl}?`;
+        if (selectedLangFilter.length > 0) {
+          selectedLangFilter.forEach((row) => {
+            if (url.slice(-1) === '?') {
+              url += `lang=${row.code}`;
+            } else {
+              url += `&lang=${row.code}`;
+            }
+          });
+        }
+        if (selectedTypeFilter.length > 0) {
+          selectedTypeFilter.forEach((row) => {
+            if (url.slice(-1) === '?') {
+              url += `subject=${row.name}`;
+            } else {
+              url += `&subject=${row.name}`;
+            }
         });
-    };
+      }
+      } else {
+        url = `${baseUrl}?subject=Bible&lang=en`;
+      }
+      // url = 'https://git.door43.org/api/catalog/v5/search?subject=Aligned%20Bible&subject=Bible&lang=en&lang=ml&lang=hi';
+      await fetch(url)
+      .then((response) => response.json())
+      .then((res) => {
+          // console.log('fetched out res ->', res.data);
+          const temp_resource = {};
+          res.data.forEach((element) => {
+              element.isChecked = false;
+              if (element.language in temp_resource) {
+                  temp_resource[element.language].push(element);
+              } else {
+                  temp_resource[element.language] = [element];
+              }
+          });
+          setresourceData(temp_resource);
+          setLoading(false);
+      });
+  };
 
     const handleCheckbox = (e, obj) => {
       const temp_resource = resourceData;
@@ -114,7 +141,6 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
 
     const handleClickFilter = () => {
       if (!loading) {
-        console.log('clicked filter : ', loadFilterDiv);
         setLoadFilterDiv(!loadFilterDiv);
       }
     };
@@ -124,8 +150,24 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
       setSelectedTypeFilter([]);
     };
 
+    const handleSaveFilter = async () => {
+      setLoadFilterDiv(!loadFilterDiv);
+      if (selectedLangFilter.length > 0 || selectedTypeFilter.length > 0) {
+        await fetchResource(true);
+        // .then(() => {
+        //   // clear filter after fetch finish
+        // setSelectedLangFilter([]);
+        // setSelectedTypeFilter([]);
+        // });
+      } else {
+        setOpenSnackBar(true);
+        setNotify('warning');
+        setSnackText('No filter applied, please select filter');
+      }
+    };
+
     React.useEffect(() => {
-        fetchResource();
+        fetchResource(false);
     }, []);
 
     const classes = useStyles();
@@ -151,18 +193,18 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
           >
             <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
 
-            <div className="flex items-center justify-center h-screen">
-              <div className="flex-col w-2/5 max-h-[32rem] items-center justify-center  z-50 shadow rounded bg-white">
+            <div className="flex items-center justify-center h-screen ">
+              <div className="flex-col w-2/5 max-h-[32rem] items-center justify-center  z-50 shadow rounded bg-white ">
 
-                <div className="w-full bg-secondary text-center text-white p-1 rounded-t">
-                  <div aria-label="resources-download-title" className="z-50 flex uppercase justify-center p-2 text-xs tracking-widest leading-snug">
+                <div className="w-full bg-secondary text-center text-white p-1 rounded-t ">
+                  <div aria-label="resources-download-title" className="z-50  flex uppercase justify-center p-2 text-xs tracking-widest leading-snug">
                     {selectResource}
                     {' '}
                     Resource Collection
                   </div>
                 </div>
 
-                <div className="w-full bg-white mt-2 p">
+                <div className="w-full bg-white mt-2 p ">
                   <div aria-label="resources-download-filter" className="z-50 flex justify-between  p-2  ">
                     <span className="text-sm font-medium">Select Resources to Download </span>
                     <div className="flex gap-4">
@@ -181,8 +223,8 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
                   </div>
                   <hr />
                   {/* filter / status section show on conditions */}
-                  {!loadFilterDiv && (
-                    <div className="flex-col border-2 m-2 border-gray-300 bg-gray-200">
+                  {loadFilterDiv && (
+                    <div className="flex-col  border-2 m-2 border-gray-300 bg-gray-200">
                       <div className="w-full flex justify-center text-sm py-1">Filter Options</div>
 
                       <div className=" flex-col text-sm p-2 ">
@@ -203,8 +245,20 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
                           />
                         </div>
                         <div className="flex justify-end mt-5 gap-5 px-5">
-                          <button type="button" onClick={handleClearFilter}>Clear</button>
-                          <button type="button">Save</button>
+                          <button
+                            type="button"
+                            className="w-20 h-8  bg-error leading-loose rounded shadow text-xs font-base  text-white tracking-wide  font-light uppercase"
+                            onClick={handleClearFilter}
+                          >
+                            {t('btn-clear')}
+                          </button>
+                          <button
+                            type="button"
+                            className="w-20 h-8  bg-success leading-loose rounded shadow text-xs font-base  text-white tracking-wide  font-light uppercase"
+                            onClick={handleSaveFilter}
+                          >
+                            {t('btn-save')}
+                          </button>
                         </div>
                       </div>
                     </div>
@@ -234,19 +288,21 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
                               <AccordionDetails>
 
                                 <div className="w-full">
-                                  <div className="grid grid-cols-5 gap-2 text-center">
-                                    <div />
-                                    <div className="col-span-2 font-medium">Resource</div>
-                                    <div className="col-span-2 font-medium">Organization</div>
+                                  <div className="grid grid-cols-8 gap-2 text-center">
+                                    <div className="col-span-1" />
+                                    <div className="col-span-1 font-medium">Resource</div>
+                                    <div className="col-span-3 font-medium">Type</div>
+                                    <div className="col-span-3 font-medium">Organization</div>
                                   </div>
                                   <hr />
                                   {resourceData[element].map((row) => (
-                                    <div className="grid grid-cols-5 gap-2 text-center p-1.5 text-sm">
+                                    <div className="grid grid-cols-8 gap-2 text-center p-1.5 text-sm">
                                       <div>
-                                        <input type="CheckBox" checked={row.isChecked} className="" onChange={(e) => handleCheckbox(e, { selection: 'single', id: row.id, parent: element })} />
+                                        <input className="col-span-1" type="CheckBox" checked={row.isChecked} onChange={(e) => handleCheckbox(e, { selection: 'single', id: row.id, parent: element })} />
                                       </div>
-                                      <div className="col-span-2">{row.name}</div>
-                                      <div className="col-span-2">{row.owner}</div>
+                                      <div className="col-span-1">{row.name}</div>
+                                      <div className="col-span-3">{row.subject}</div>
+                                      <div className="col-span-3">{row.owner}</div>
                                     </div>
                                 ))}
                                 </div>
