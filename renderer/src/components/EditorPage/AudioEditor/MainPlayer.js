e7c03c7128bb7dbe38ec0e488aabb1555cdee637
vipinpaul <vipin.paul@bridgeconn.com>
2022-11-14 20:32:29 +0530

    Fixing the issue with takes UI

--- renderer/src/components/EditorPage/AudioEditor/MainPlayer.js
+++ renderer/src/components/EditorPage/AudioEditor/MainPlayer.js
@@ -46,6 +46,7 @@ const MainPlayer = () => {
       ([key]) => {
         if (audioContent[key].verseNumber === verse) {
           setCurrentUrl(audioContent[key]);
+          setTrigger('url');
         }
       },
     );
