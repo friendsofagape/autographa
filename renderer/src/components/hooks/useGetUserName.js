08f729194d770270fd3706de52e3dcf05bf0f7b3
Bobby <bobbykumar706584@gmail.com>
2022-12-05 17:00:17 +0530

    lint fix

--- renderer/src/components/hooks/useGetUserName.js
+++ renderer/src/components/hooks/useGetUserName.js
@@ -1,25 +1,17 @@
 import { useEffect, useState } from 'react';
-import localforage from 'localforage';
 import { isElectron } from '@/core/handleElectron';
-import { useRouter } from 'next/router';
+import * as localforage from 'localforage';
 import * as logger from '../../logger';
 
 // custom hook to fetch username from localforage
 export const useGetUserName = (_username) => {
     const [username, setUsername] = useState('');
-    const router = useRouter()
     useEffect(() => {
         const fetchUserName = async () => {
             try {
                 if (!username && isElectron()) {
                     const value = await localforage.getItem('userProfile');
-                    if (value?.username) {
-                        setUsername(value?.username)
-                    }else{
-                        localForage.removeItem('sessionToken');
-                        localForage.removeItem('userProfile');
-                        router.push('/')
-                    }
+                    setUsername(value?.username);
                 }
             } catch (error) {
                 logger.error('useGetUserName.js', error);
