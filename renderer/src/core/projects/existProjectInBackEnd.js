d0de1ffa05445112fbbd45ad8fae2706fe845d4f
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-01-19 10:54:46 +0530

    Deleted resource loading issue in another project

--- renderer/src/core/projects/existProjectInBackEnd.js
+++ renderer/src/core/projects/existProjectInBackEnd.js
@@ -9,17 +9,12 @@ function isBackendProjectExist(ProjectDir) {
     return new Promise((resolve) => {
         // let checkStatus = false;
         localforage.getItem('userProfile').then((value) => {
-            console.log('inside local =====> 1');
             if (value?.username) {
-                console.log('inside if =====> 2');
                 const resourcePath = path.join(newpath, 'autographa', 'users', value.username, 'resources', ProjectDir);
-                console.log('DIR : ', { ProjectDir });
                 // check for path exist or not and resolve true or false will work for pane 1 now add for other panes
                 if (fs.existsSync(resourcePath) && fs.existsSync(path.join(resourcePath, 'metadata.json'))) {
-                    console.log('inside exist path =====> 3');
                     resolve(true);
                 } else {
-                    console.log('inside else faield exist ========');
                     resolve(false);
                 }
             } else {
